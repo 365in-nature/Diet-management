db9d7dc894bba3dcfbec0320afc0fef0c762ef6d
365in-nature <jsg.lcc@gmail.com>
2026-04-22 15:32:51 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 // =============================================
 // CONFIGURATION - Supabase 연결 정보 입력 필요
 // =============================================
-const SUPABASE_URL = "https://agjwbhsrjhdhegtkoffy.supabase.co/rest/v1/";
+const SUPABASE_URL = "https://agjwbhsrjhdhegtkoffy.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFnandiaHNyamhkaGVndGtvZmZ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY4MDg5NzAsImV4cCI6MjA5MjM4NDk3MH0._wEMNazSLVebZblCLXLw1yEc8Exr0nYu3y4Rj7WMmc8";
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
